bb7e871574e2ee587e72702d473df44c9aba2980
kim20210612 <kim.20210612@gmail.com>
2026-07-20 22:34:04 +0900

    Delete guestbook-config.js

--- guestbook-config.js
+++
@@ -1,5 +0,0 @@
-// 방명록 저장소 설정 (Supabase 연동됨)
-window.GUESTBOOK_CONFIG = {
-  supabaseUrl: "https://udbzjuesvwfvcqkgbxsq.supabase.co",
-  supabaseAnonKey: "sb_publishable_AfgYAiFnmJaMsjuS6bs4vA_f8tjit8G",
-};
